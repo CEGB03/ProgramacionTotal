a3318f7c35cb992d329a1aca10b6e659cf148c43
CEGB03 <cgomez453@alumnos.iua.edu.ar>
2024-02-07 15:08:43 -0300

    reorganization Experimentaciones

---
+++ Experimentaciones/Java/SanValentin/src/main/java/logica/main.java
@@ -0,0 +1,18 @@
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author CEGB03
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        System.out.println("Adios mundo");
+    }
+    
+}
